c0dcb8014c5bb55f3b2dd63f835edf6a4c1ae87c
Erhan Cagirici <erhan@upbound.io>
2026-05-02 13:59:12 +0300

    generate: bump crossplane-runtime v2.2.1
    Signed-off-by: Erhan Cagirici <erhan@upbound.io>

--- internal/controller/namespaced/null/resource/zz_controller.go
+++ internal/controller/namespaced/null/resource/zz_controller.go
@@ -65,8 +65,7 @@ func Setup(mgr ctrl.Manager, o tjcontroller.Options) error {
 	// register webhooks for the kind v1alpha1.Resource
 	// if they're enabled.
 	if o.StartWebhooks {
-		if err := ctrl.NewWebhookManagedBy(mgr).
-			For(&v1alpha1.Resource{}).
+		if err := ctrl.NewWebhookManagedBy(mgr, &v1alpha1.Resource{}).
 			Complete(); err != nil {
 			return errors.Wrap(err, "cannot register webhook for the kind v1alpha1.Resource")
 		}
